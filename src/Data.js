12b8ab6050458d884b49218d0bd03b2bb0585b37
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-06 21:27:09 -0500

    updat

---
+++ src/Data.js
@@ -0,0 +1,29 @@
+const books = [
+  {
+    title: "book 1",
+    img: "img",
+    desc: "fgdsgfsgfgsfdgsxc",
+  },
+  {
+    title: "book 2",
+    img: "img",
+    desc: "knjglkfgjflkgj",
+  },
+  {
+    title: "book3",
+    img: "img",
+    desc: "lkjflkgdflgkfg",
+  },
+  {
+    title: "boook 4",
+    img: "img",
+    desc: "lkjlkfgjfdlgjf",
+  },
+  {
+    title: "book 5 ",
+    img: "img",
+    desc: "kjfsgdlkdjfglkf",
+  },
+];
+
+export default books;
